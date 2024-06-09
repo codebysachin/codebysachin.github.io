dc90af8ed26980aaf943e0e1245b1ac5f10858c4
Sachin Gupta <codebysachin@gmail.com>
2024-06-09 11:39:52 +0100

    refactor

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Sachin Gupta",
+  EMAIL: "codebysachin@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Sachin Gupta official blog and portfolio.",
 };
 
 export const BLOG: Metadata = {
@@ -31,11 +31,11 @@ export const PROJECTS: Metadata = {
 export const SOCIALS: Socials = [
   { 
     NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://twitter.com/codebysachin",
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/codebysachin"
   },
   { 
     NAME: "linkedin",
---
+++ src/content/work/nec.md
@@ -0,0 +1,7 @@
+---
+company: "NEC"
+role: "Lead Software Engineer"
+dateStart: "07/10/2023"
+dateEnd: "Current"
+---
+As Lead Software Engineer at NEC, I spearheaded projects for a government client, managing a team of 5 developers. My responsibilities included decision-making on data design, story estimation, solution design, code review, ensuring code quality, and liaising with business stakeholders to grasp domain intricacies.
